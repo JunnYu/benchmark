055f2391e2065c87132e5c96f1595394eb921a6f
yujun <573009727@qq.com>
2023-07-13 14:55:11 +0800

    update train code

--- frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/sd/benchmark_common/bf16.sh
+++ frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/sd/benchmark_common/bf16.sh
@@ -1,10 +1,19 @@
-export BATCH_SIZE=64
-export MAX_ITER=200000
+# unset PADDLE_ELASTIC_JOB_ID
+# unset PADDLE_TRAINER_ENDPOINTS
+# unset DISTRIBUTED_TRAINER_ENDPOINTS
+# unset FLAGS_START_PORT
+# unset PADDLE_ELASTIC_TIMEOUT
 
+export LD_LIBRARY_PATH=/usr/local/cuda/compat:$LD_LIBRARY_PATH
+
+export FLAG_USE_EMA=0
 export FLAG_RECOMPUTE=1
 export FLAG_BENCHMARK=1
 export FLAG_USE_EMA=1
+
 export OUTPUT_DIR="bf16_torch"
+export BATCH_SIZE=64
+export MAX_ITER=200000
 
 node_num=${PADDLE_TRAINERS_NUM}
 num_workers=8
@@ -12,7 +21,7 @@ node_rank=${PADDLE_TRAINER_ID}
 master_addr=${POD_0_IP}
 master_port=14233
 
-torchrun --nnodes ${node_num} --nproc_per_node ${num_workers} --node_rank ${node_rank} --master_addr ${master_addr} --master_port ${master_port} train_txt2img_laion400m_trainer.py \
+nohup python torchrun --nnodes ${node_num} --nproc_per_node ${num_workers} --node_rank ${node_rank} --master_addr ${master_addr} --master_port ${master_port} train_txt2img_laion400m_trainer.py \
     --do_train \
     --output_dir ${OUTPUT_DIR} \
     --per_device_train_batch_size ${BATCH_SIZE} \
@@ -22,6 +31,7 @@ torchrun --nnodes ${node_num} --nproc_per_node ${num_workers} --node_rank ${node
     --max_steps ${MAX_ITER} \
     --lr_scheduler_type "constant" \
     --warmup_steps 0 \
+    --image_logging_steps 1000 \
     --logging_steps 10 \
     --resolution 256 \
     --save_steps 10000 \
@@ -35,7 +45,6 @@ torchrun --nnodes ${node_num} --nproc_per_node ${num_workers} --node_rank ${node
     --disable_tqdm True \
     --overwrite_output_dir True \
     --optim adamw_torch \
-    --tf32 True  \
-    --image_logging_steps 1000 \
+    --tf32 True \
     --bf16 True \
-    --ddp_find_unused_parameters False
+    --ddp_find_unused_parameters False > torch_sd_bf16_2048.log 2>&1 &
